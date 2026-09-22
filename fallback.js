2cf69e071362510071d8ccee5c564c91042b8b1d
skid <ar.dalfr@gmail.com>
2026-09-22 11:45:04 +0100

    Delete fallback.js

--- fallback.js
+++
@@ -1,8 +0,0 @@
-const fs=require("fs")
-const p=require("../pretty")
-const r=require("../repair")
-const f=process.argv[2]
-const l=process.argv[3]||"luac"
-let s=fs.readFileSync(f,"utf8")
-fs.writeFileSync(f,p.run(s))
-try{r.run(f,l)}catch(e){}
